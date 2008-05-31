084a5b45808b8daeb165aafdb3d1e3e551cb28df
Michael Dominic K <mdk@mdk.am>
2008-05-31 20:54:54 +0200

    Adding some number boxing code example to NSNumber objc example.

--- objc/numbers/main.m
+++ objc/numbers/main.m
@@ -7,12 +7,14 @@ int main (int argc, const char *argv[])
 {
   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
+  // Boxing numbers as objects
   NSNumber *v1 = [NSNumber numberWithInt: 32];
   NSNumber *v2 = [NSNumber numberWithFloat: 32.32];
   NSNumber *v3 = [NSNumber numberWithChar: 'q'];
 
   NSArray *array = [NSArray arrayWithObjects: v1, v2, v3, nil];
 
+  printf ("Listing various numbers...\n");
   int n = 1;
   id e = [array objectEnumerator];
   id obj;
@@ -20,6 +22,18 @@ int main (int argc, const char *argv[])
     printf ("%d: %s\n", n, [[obj description] UTF8String]);
     n++;
   }
+
+  // Accessing the unboxed values
+  printf ("Accessing boxed values...\n");
+  printf ("1: %d\n", [v1 intValue]);
+  printf ("2: %f\n", [v2 floatValue]);
+  printf ("3: %d\n", [v3 charValue]);
+
+  // Conversions
+  printf ("Auto-converting values...\n");
+  printf ("1: %f\n", [v1 floatValue]);
+  printf ("2: %d\n", [v2 intValue]);
+  printf ("3: %f\n", [v3 doubleValue]);
 
   [pool release];
   return 0;
