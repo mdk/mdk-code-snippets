fd4386bdb9b84d167acd19f7d514737d8475ff67
Michael Dominic K <mdk@mdk.am>
2009-01-01 18:36:32 +0100

    Adding an example for XML building using builder.

---
+++ ruby/xml-builder/builder.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'builder/xmlmarkup'
+
+f = File.open('output.xml', 'w')
+xml = Builder::XmlMarkup.new(:target => f, :indent => 1)
+
+# Simple metadata
+xml.instruct!
+xml.comment! "This is a sample output document"
+
+# Simple elements writing
+xml.title "This is a title element"
+xml.author "Michael Dominic K.", :gender => "male"
+
+# Complex/nested elements writing
+xml.content {
+  xml.page1 "First piece"
+  xml.page2 "Second piece"
+  xml.page3 "Third piece"
+  xml.cover "Cover string", :material => "cardboard"
+}
+
